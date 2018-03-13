4034375955aac780c049b0a690fbb203f47d2066
Radek <radekcz.dev@gmail.com>
2018-03-13 13:39:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     element.find('[data-customer-phoneNumber-section] [data-customer-phoneNumber-input]').val(data.phoneNumber);
     
     element.find('[data-customer-address-section] [data-customer-address-city-paragraph]').text(data.address.city);
-    element.find('[data-customer-address-section] [data-customer-address-city-input]').val(data.adress.city);
+    element.find('[data-customer-address-section] [data-customer-address-city-input]').val(data.address.city);
 
     return element;
   }
