f580dafcc68d6285a1da26f05f7d76525c9eaf3b
Radek <radekcz.dev@gmail.com>
2018-03-13 13:02:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,38 @@ $(document).ready(function() {
 
   var apiRoot = 'https://fathomless-falls-89522.herokuapp.com/customer/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-  var tasksContainer = $('[data-tasks-container]');
+  var customersContainer = $('[data-tasks-container]');
 
   // init
-  getAllTasks();
+  getAllCustomers();
 
   function createElement(data) {
     var element = $(datatableRowTemplate).clone();
 
-    element.attr('data-task-id', data.id);
-    element.find('[data-task-name-section] [data-task-name-paragraph]').text(data.name);
-    element.find('[data-task-name-section] [data-task-name-input]').val(data.name);
+    element.attr('data-customer-id', data.id);
+    element.find('[data-customer-name-section] [data-customer-name-paragraph]').text(data.name);
+    element.find('[data-customer-name-section] [data-customer-name-input]').val(data.name);
 
-    element.find('[data-task-content-section] [data-task-content-paragraph]').text(data.surname);
-    element.find('[data-task-content-section] [data-task-content-input]').val(data.surname);
+    element.find('[data-customer-surname-section] [data-customer-surname-paragraph]').text(data.surname);
+    element.find('[data-customer-surname-section] [data-customer-surname-input]').val(data.surname);
+    
+    element.find('[data-customer-email-section] [data-customer-email-paragraph]').text(data.email);
+    element.find('[data-customer-email-section] [data-customer-email-input]').val(data.email);
+    
+    element.find('[data-customer-phoneNumber-section] [data-customer-phoneNumber-paragraph]').text(data.phoneNumber);
+    element.find('[data-customer-phoneNumber-section] [data-customer-phoneNumber-input]').val(data.phoneNumber);
 
     return element;
   }
 
   function handleDatatableRender(data) {
-    tasksContainer.empty();
-    data.forEach(function(task) {
-      createElement(task).appendTo(tasksContainer);
+    customersContainer.empty();
+    data.forEach(function(customer) {
+      createElement(customer).appendTo(customersContainer);
     });
   }
 
-  function getAllTasks() {
+  function getAllCustomers() {
     var requestUrl = apiRoot + 'getCustomers';
 
     $.ajax({
@@ -118,8 +124,8 @@ $(document).ready(function() {
 
   $('[data-task-add-form]').on('submit', handleTaskSubmitRequest);
 
-  tasksContainer.on('click','[data-task-edit-button]', toggleEditingState);
-  tasksContainer.on('click','[data-task-edit-abort-button]', toggleEditingState);
-  tasksContainer.on('click','[data-task-submit-update-button]', handleTaskUpdateRequest);
-  tasksContainer.on('click','[data-task-delete-button]', handleTaskDeleteRequest);
+  customersContainer.on('click','[data-task-edit-button]', toggleEditingState);
+  customersContainer.on('click','[data-task-edit-abort-button]', toggleEditingState);
+  customersContainer.on('click','[data-task-submit-update-button]', handleTaskUpdateRequest);
+  customersContainer.on('click','[data-task-delete-button]', handleTaskDeleteRequest);
 });
